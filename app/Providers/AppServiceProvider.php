f5bef9394907322d6212db12c779b5be7757ce5f
lordrickategeka <lordrickategeka@gmail.com>
2022-01-31 19:22:49 +0300

    initial commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,31 +1,24 @@
 <?php
-
 namespace App\Providers;
-
-use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
-
-class AppServiceProvider extends ServiceProvider
-{
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //add this
+//import this
+use Illuminate\Support\Facades\Schema;
+class AppServiceProvider extends ServiceProvider{
+  /**
+  * Register any application services.
+  *
+  * @return void
+  */
+  public function register(){
+     //add this
      Schema::defaultStringLength(191);
-
-    }
-
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
-    }
+  }
+  /**
+  * Bootstrap any application services.
+  *
+  * @return void
+  */
+   public function boot(){
+      //
+   }
 }
